250365d67a75c1a2bd3dff18e7db08755f4f479a
Alex <leha.maslennikov2003@gmail.com>
2023-11-09 19:16:29 +0500

    TelegramGameProcessor::callback

--- src/main/java/com/main/Main.java
+++ src/main/java/com/main/Main.java
@@ -2,13 +2,17 @@ package com.main;
 
 import com.bot.Bot;
 import com.bot.dispatcher.Dispatcher;
-import com.bot.dispatcher.Event;
 import com.bot.dispatcher.filters.Command;
+import com.bot.dispatcher.filters.State;
+import com.bot.dispatcher.handlers.CallbackQueryHandler;
 import com.bot.dispatcher.handlers.MessageHandler;
+import com.gameprocessor.Box;
+import com.gameprocessor.Equipment;
 import com.gameprocessor.GameProcessor;
 import com.gameprocessor.Sendable;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.AnswerCallbackQuery;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
@@ -49,7 +53,7 @@ public class Main {
                                             tgp.start();
                                         }
                                         else{
-                                            var tgp = new TelegramGameProcessor(bot, event.event.getMessage().getChatId());
+                                            var tgp = new TelegramGameProcessor(bot, event.event.getMessage().getChatId(), event.userId);
                                             event.setData("game", tgp);
                                             tgp.start();
                                         }
@@ -134,6 +138,33 @@ public class Main {
                 )
         );
 
+        dp.addHandler(
+                new CallbackQueryHandler(
+                        new State("send"),
+                        event -> {
+                            event.getData("game").thenAccept(
+                                    object -> {
+                                        if(object instanceof TelegramGameProcessor tgp){
+                                            tgp.callback(event.event.getCallbackQuery().getData());
+                                        }
+                                    }
+                            );
+                            try {
+                                event.bot.executeAsync(
+                                        AnswerCallbackQuery.builder()
+                                                .callbackQueryId(
+                                                        event.event.getCallbackQuery().getId()
+                                                )
+                                                .build()
+                                );
+                            }
+                            catch (Exception e){
+                                e.printStackTrace();
+                            }
+                        }
+                )
+        );
+
         return dp;
     }
 
@@ -147,9 +178,13 @@ public class Main {
 class TelegramGameProcessor extends GameProcessor {
     private final Bot bot;
     private final long chatId;
-    public TelegramGameProcessor(Bot bot, Long chatId){
+    private final long userId;
+    private List<? extends Sendable> data;
+    private Sendable chosen;
+    public TelegramGameProcessor(Bot bot, long chatId, long userId){
         this.bot = bot;
         this.chatId = chatId;
+        this.userId = userId;
     }
     /**
      * Отправляет текстовое сообщение пользователю
@@ -179,7 +214,9 @@ class TelegramGameProcessor extends GameProcessor {
     @Override
     public void send(String text, List<? extends Sendable> obj) {
         try {
-            bot.setUserData(chatId, "sendable", obj);
+            //bot.setUserData(userId, "sendable", obj);
+            data = obj;
+            bot.setUserData(userId, "state", "send");
             Integer n = 0;
             List<List<InlineKeyboardButton>> kb = new LinkedList<>();
             for(Sendable i: obj){
@@ -191,6 +228,7 @@ class TelegramGameProcessor extends GameProcessor {
                                 .build()
                         );
                 kb.add(row);
+                n++;
             }
             bot.executeAsync(
                     SendMessage.builder().chatId(this.chatId).text(text)
@@ -208,6 +246,45 @@ class TelegramGameProcessor extends GameProcessor {
 
     @Override
     public void callback(String callbackData) {
+        if(callbackData.equals("loot") && chosen instanceof Box box){
+            loot(box);
+            return;
+        }
+        if(callbackData.equals("equip") && chosen instanceof Equipment equipment){
+            equip(equipment);
+            return;
+        }
+        if(callbackData.equals("unequip") && chosen instanceof Equipment equipment){
+            unequip(equipment);
+            return;
+        }
+        try {
+            Integer n = Integer.parseInt(callbackData);
+            List<List<InlineKeyboardButton>> kb = new LinkedList<>();
+            Sendable obj = data.get(n);
+            chosen = obj;
+            for(String i: obj.getActions().getActions()){
+                List<InlineKeyboardButton> row = new LinkedList<>();
+                row.add(
+                        InlineKeyboardButton.builder()
+                                .text(i)
+                                .callbackData(i)
+                                .build()
+                );
+                kb.add(row);
+            }
+            bot.executeAsync(
+                    SendMessage.builder().chatId(this.chatId).text(obj.getLongText())
+                            .replyMarkup(
+                                    InlineKeyboardMarkup.builder()
+                                            .keyboard(kb)
+                                            .build()
+                            ).build()
+            );
 
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
